b3b3151964f24cdaadf17e2d242e7b0481bac97a
Ilya Lukichev <zerg959@yandex.ru>
2022-09-02 18:33:28 +0300

    workflow_1

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['84.201.139.255']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
