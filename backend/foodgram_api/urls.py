682ad63fd1cb17fe25b0e81a5cff5aa2b89b3dd2
Ilya Lukichev <zerg959@yandex.ru>
2022-09-05 01:10:14 +0300

    7

--- backend/foodgram_api/urls.py
+++ backend/foodgram_api/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html')
-    ),
+    # path(
+    #     'redoc/',
+    #     TemplateView.as_view(template_name='redoc.html')
+    # ),
     path('', include('api.urls'))]
