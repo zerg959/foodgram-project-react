aa0173d7421bdd5871af79cfb9d8625e93490fea
Ilya Lukichev <zerg959@yandex.ru>
2022-09-04 19:05:47 +0300

    fianl

---
+++ backend/data/load_ingredients.py
@@ -0,0 +1,26 @@
+import csv
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        data_path = settings.BASE_DIR
+        with open(
+            f'./ingredients.csv', 'r', encoding='utf-8'
+        ) as file:
+            file_reader = csv.reader(file)
+            data = []
+            for row in file_reader:
+                name, measurement_unit = row
+                ingredient = Ingredient(
+                    name=name,
+                    measurement_unit=measurement_unit
+                )
+                data.append(ingredient)
+            Ingredient.objects.bulk_create(data)
