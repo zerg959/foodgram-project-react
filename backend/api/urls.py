c71ed95d6c82c451b2d7c85fcd16485bb04fc4e9
Ilya Lukichev <zerg959@yandex.ru>
2022-09-05 03:48:10 +0300

    13

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import SimpleRouter
 from users.views import UserViewSet
 
 from .views import IngredientsViewSet, RecipesViewSet, TagsViewSet
+from django.views.generic import TemplateView
 
 router = SimpleRouter()
 router.register(r'tags', TagsViewSet)
@@ -14,6 +15,10 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('users/', include('users.urls')),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html')
+    ),
     path('', include('djoser.urls.authtoken')),
     path('', include(router.urls)),
 ]
