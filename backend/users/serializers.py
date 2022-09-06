93193928aab30763589612b43e0833b78a326398
Ilya Lukichev <zerg959@yandex.ru>
2022-09-06 09:06:58 +0300

    16

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -106,6 +106,10 @@ class PasswordChangeSerializer(serializers.Serializer):
     current_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
 
+    def validate_curren_password(self, current_password):
+        password_validation.validate_password(current_password, self.instance)
+        return current_password
+
     def validate_new_password(self, new_password):
         password_validation.validate_password(new_password, self.instance)
         return new_password
