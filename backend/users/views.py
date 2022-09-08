c4970df517905b53a6a690e7bab243a156f0dee7
Ilya Lukichev <zerg959@yandex.ru>
2022-09-09 01:16:57 +0300

    change psw message2

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,9 @@
+from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.permissions import SAFE_METHODS, AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
-from django.contrib import messages
 
 from api.pagination import CustomPagination
 from users.models import Subscription, User
