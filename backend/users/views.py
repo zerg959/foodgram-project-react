f3ea0586822edf70abe102c205885a2f9663c701
Ilya Lukichev <zerg959@yandex.ru>
2022-09-09 01:27:40 +0300

    change psw message3

--- backend/users/views.py
+++ backend/users/views.py
@@ -54,10 +54,9 @@ class ChangePasswordView(viewsets.ModelViewSet):
                 {"current_password": ["Wrong password"]},
                 status=status.HTTP_400_BAD_REQUEST
             )
-        if self.object.set_password(
-                serializer.validated_data.get("new_password")):
-            return messages.success(
-                request, 'Password changed successfully!')
+        self.object.set_password(serializer.validated_data.get("new_password"))
+        # return messages.success(
+        #         request, 'Password changed successfully!')
         self.object.save()
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
